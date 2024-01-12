69c28e00410c9b2002af62087f725cf54afa8d2e
Priyanshu Gupta <priyanshu.g@pepperfry.com>
2024-01-13 00:14:35 +0530

    middlewares created & routes added

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,7 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
 
 const createUser = asyncHandler(async (req, res) => {
   console.log("createUser controller", req.body);
@@ -13,45 +14,142 @@ const createUser = asyncHandler(async (req, res) => {
     return res.json(newUser);
   } else {
     // user already exist
-    throw new Error("User already exist")
+    throw new Error("User already exist");
   }
 });
 
-const loginUserCtrl = asyncHandler(async(req,res)=>{
-
-  const {email, password} = req.body;
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
   // check if user exists or not
-  const findUser = await User.findOne({email});
-  if(findUser && await findUser.isPasswordMatched(password)){
-
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
     res.json({
-      _id : findUser._id,
-      firstname : findUser?.firstname,
-      lastname : findUser?.lastname,
-      email : findUser?.email,
-      mobile : findUser?.mobile,
-      token : generateToken(findUser?._id)
-    })
-
-  }else{
-    throw new Error("Invalid Credentials")
+      _id: findUser._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("Invalid Credentials");
   }
+});
 
-})
+// update a user
 
+const updatedUser = asyncHandler(async (req, res) => {
+  const { _id } = req?.user;
+  validateMongoDbId(_id);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        firstname: req?.body?.firstname,
+        lastname: req?.body?.lastname,
+        email: req?.body?.email,
+        mobile: req?.body?.mobile,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 // Get all users
-const getAllUser = asyncHandler(async(req, res)=>{
-
-  try{
-    const getUsers = await User.find()
-    res.json(getUsers)
-
-
-  } catch(error){
-    throw new Error(error)
+const getAllUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
   }
+});
 
-})
+// get a single user
+const getaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaUser = await User.findById(id);
+    res.json({
+      getaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-module.exports = { createUser,loginUserCtrl, getAllUser };
+//  delete a user
+const deleteaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    console.log("delete id", id);
+    const deleteaUser = await User.findByIdAndDelete(id);
+    res.json({
+      deleteaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const blockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const block = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "User Blocked",
+      updated: block,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const unblockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const unblock = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: false,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "User Unblocked",
+      updated: unblock,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+  getaUser,
+  deleteaUser,
+  updatedUser,
+  blockUser,
+  unblockUser,
+};
